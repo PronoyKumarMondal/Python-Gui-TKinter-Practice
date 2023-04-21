03a8317f8d2cae1078b7a4c13349f8481d78c73f
PronoyKumarMondal <pronoy15-14744@diu.edu.bd>
2023-04-22 02:40:15 +0600

    radiobox getting radiobox value createing intvariable anchoring

--- GUIBox.py
+++ GUIBox.py
@@ -13,11 +13,22 @@ my_label.pack()#packing /endling
 my_label2.pack()
 def myclick():
     my_label=Label(root,text='Welcome to DeadHead '+e.get(),fg='red').pack()#fg - foreground,bg-background ,e.get()- means getting the string we write the entrybox
-    e.delete(0,END)
+    e.delete(0, END)#after clicking 1 time the value is clear
 mybutton=Button(root,text='Hey click this',command=myclick,fg='white',bg='black',padx=30,pady=30)#clicking button,padx-padding sizing the button
 mybutton.pack()
 e=Entry(root,width=50,fg='red')#entry box
 e.pack()
+
+#radiobutton
+q=IntVar()
+q.get()
+q.set('2')
+def click(value):
+    my_label3=Label(root,text=value).pack()
+Radiobutton(root,text='option1',variable=q ,value=1).pack(anchor='w')
+Radiobutton(root,text='option2',variable=q ,value=2).pack(anchor='w')
+my_label3=Label(root,text=q.get()).pack(anchor='w')
+mybutton1= Button(root,text='submit',command=lambda :click(q.get())).pack(anchor='w')
 
 
 root.mainloop()
