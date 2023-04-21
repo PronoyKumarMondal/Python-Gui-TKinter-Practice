f629620da93d08727c4d79845af22fcf3989605b
PronoyKumarMondal <pronoy15-14744@diu.edu.bd>
2023-04-22 02:12:44 +0600

    Entry box and getting the entry

--- GUIBox.py
+++ GUIBox.py
@@ -12,9 +12,11 @@ my_label.grid(row=0,column=1)
 my_label.pack()#packing /endling
 my_label2.pack()
 def myclick():
-    my_label=Label(root,text='Welcome to DeadHead,',fg='red').pack()#fg - foreground,bg-bagroung
-mybutton=Button(root,text='Hey click this',command=myclick,fg='white',bg='black',padx=30,pady=30)#clicking button
+    my_label=Label(root,text='Welcome to DeadHead '+e.get(),fg='red').pack()#fg - foreground,bg-background ,e.get()- means getting the string we write the entrybox
+mybutton=Button(root,text='Hey click this',command=myclick,fg='white',bg='black',padx=30,pady=30)#clicking button,padx-padding sizing the button
 mybutton.pack()
+e=Entry(root,width=50,fg='red')#entry box
+e.pack()
 
 
 root.mainloop()
